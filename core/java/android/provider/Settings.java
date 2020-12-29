bd06ac991eaac1ad0a8a47d62c42ffc51ac32fbf
calebcabob <calphonic@gmail.com>
2020-12-29 18:42:52 +0900

    base: Introduce QS tile style picker [1/3]
    Stallix edit: Forward port to 11
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5809,6 +5809,11 @@ public final class Settings {
          * @hide
          */
         public static final String QS_HEADER_STYLE = "qs_header_style";
+		
+		/**
+         * @hide
+         */
+        public static final String QS_TILE_STYLE = "qs_tile_style";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -6001,6 +6006,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_POSITION);
             PRIVATE_SETTINGS.add(PROXIMITY_ON_WAKE);
 			PRIVATE_SETTINGS.add(QS_HEADER_STYLE);
+			PRIVATE_SETTINGS.add(QS_TILE_STYLE);
         }
 
         /**
